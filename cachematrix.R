04d506ef3eb91a1b774cfb172d9f188861a46a6a
davidreispt <davidreispt@gmail.com>
2014-10-22 22:31:11 +0100

    Update cachematrix.R
    exercice commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow create a special object that cache a matrix and returns its inverted matrix 
 
-## Write a short comment describing this function
-
+# This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        #validates if the object is matrix
+        if (!is.matrix(x)){
+            stop("object is not a matrix")
+        
+        #validates if the matrix is squared and throws error if not
+        } else if (nrow(x)!=ncol(x)) {
+            stop("not a squared matrix")
+            
+        #nullifies m 
+        } else m <- NULL
+        
+        #set function - store matrix 
+        set <- function(y) {
+                m <- NULL
+                x <<- y
+        }
+        
+        # returns original matrix       
+        get <- function() x
+        
+        # stores the inverted matrix
+        setimatrix <- function(imatrix) m <<- imatrix
+        
+        # returns the inverted matrix
+        getimatrix <- function() m
+        
+        # returns list of function to operate cacheMatrix object
+        list(set = set, get = get,
+             setimatrix = setimatrix,
+             getimatrix = getimatrix)
 }
 
 
-## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix and returns the cached value if it exists   
 cacheSolve <- function(x, ...) {
+        # get the matrix from the funcion on object x
+        m <- x$getimatrix()
+        
+        # checks if the objects exists (was cached already)
+        if(!is.null(m)) {
+                ## Return the cached matrix that is the inverse of 'x'
+                return(m)
+        }
+        
+        # get the matrix from the funcion on object x
+        data <- x$get()
+        
+        # solves the matrix 
+        m <- solve(data, ...)
+        
+        #sets(caches) the inverse matrix on object x using the set function
+        x$setimatrix(m)
+        
         ## Return a matrix that is the inverse of 'x'
+        m
 }
